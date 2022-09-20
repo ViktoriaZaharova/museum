6a4b39160a27745d6f02a981b6f8354440e53a30
Viktoria <zaharova181114@gmail.com>
2022-09-20 13:18:21 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -56,11 +56,51 @@ $(window).on('load resize', function () {
     }
 });
 
+$('.team-projects-wrapper .container').each(function () {
+    if ($(this).find('.team-projects-item').length > 4) {
+        $(this).find('.team-projects-item').slice(4).hide();
+    }
+
+});
+
+$('.btn-toggle-team').on('click', function (e) {
+    e.preventDefault();
+    $(this).parents('.team-projects-wrapper').find('.team-projects-item:hidden').slice(0, 4).css('display', 'flex');
+
+    var onBlock = $(this).prev('.team-projects-wrapper').find('.team-projects-item:hidden').length;
+    if(onBlock <= 0) {
+        $(this).hide();
+    }
+});
+
+
 $('.product-slider').slick({
     slidesToShow: 1,
     fade: true,
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 });
+
+$('.history-museum-slider').slick({
+    slidesToShow: 3,
+    arrows: true,
+    appendArrows: '.history-museum-slider__nav',
+    variableWidth: true,
+    centerMode: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+});
+
+$('.history-museum-timeline-slider .history-museum-timeline__item').click(function() {
+    var $this = $(this);
+    $('.history-museum-timeline__item').removeClass('active');
+    $this.addClass('active');
+    $('.history-museum-slider').slick('slickGoTo', $this.data('index'))
+});
+
+// $('.history-museum-timeline').slick({
+//     slidesToShow: 12,
+//     asNavFor: '.history-museum-slider'
+// });
 
 
